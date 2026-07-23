9ad1b98e51d7e17d95f7b9345b0699c9866693ed
morningst4r25 <sitorus1255@gmail.com>
2026-07-23 14:15:18 +0700

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener('load',()=>setTimeout(()=>loader.classList.add('hide'),900));
-const io=new IntersectionObserver(e=>e.forEach(x=>x.isIntersecting&&x.target.classList.add('show')),{threshold:.15});
-document.querySelectorAll('.reveal').forEach(x=>io.observe(x));
